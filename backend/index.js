d48550b68b251c0175514a157a046c789ecdffb9
AchmadWahyu <achwahyu.w@gmail.com>
2023-10-08 00:43:46 +0700

    feat: chat to multiple user

--- backend/index.js
+++ backend/index.js
@@ -8,17 +8,35 @@ const port = 8080;
 const wss = new WebSocketServer({ server });
 
 wss.on('connection', (ws, req) => {
+  const userId = randomUUID();
+
+  wss.clients.forEach((client) => {
+    if (client.OPEN) {
+      client.send(
+        JSON.stringify({
+          id: userId,
+          text: `User ${userId} joined the chat`,
+          type: 'connection',
+          timeStamp: Date.now()
+        })
+      );
+    }
+  });
+
   ws.on('error', console.error);
 
   ws.on('message', (data) => {
     const composedChat = {
-      id: randomUUID(),
-      text: data.toString(),
+      ...JSON.parse(data),
+      timeStamp: Date.now()
     };
-    
-    ws.send(JSON.stringify(composedChat));
+
+    wss.clients.forEach((client) => {
+      if (client.OPEN) {
+        client.send(JSON.stringify(composedChat));
+      }
+    });
   });
-  
 });
 
 server.listen(port, () => {
